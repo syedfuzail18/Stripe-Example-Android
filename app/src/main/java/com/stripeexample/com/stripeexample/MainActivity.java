eb953df139c988e112142d90a19d56f168961474
Syed Fuzail <fuzail.a@finoit.co.in>
2019-06-13 18:13:35 +0530

    Bug Fixes

--- app/src/main/java/com/stripeexample/com/stripeexample/MainActivity.java
+++ app/src/main/java/com/stripeexample/com/stripeexample/MainActivity.java
@@ -1,45 +1,30 @@
 package com.stripeexample.com.stripeexample;
 
 import android.app.Activity;
-import android.inputmethodservice.Keyboard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.stripe.android.Stripe;
 import com.stripe.android.TokenCallback;
-import com.stripe.android.exception.APIConnectionException;
-import com.stripe.android.exception.APIException;
-import com.stripe.android.exception.AuthenticationException;
-import com.stripe.android.exception.InvalidRequestException;
 import com.stripe.android.model.Card;
-import com.stripe.android.model.PaymentIntent;
-import com.stripe.android.model.PaymentIntentParams;
-import com.stripe.android.model.PaymentMethodCreateParams;
 import com.stripe.android.model.Token;
-import com.stripe.android.view.CardMultilineWidget;
 
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
+/**
+ * Created by Syed Fuzail
+ * contact:
+ * byhaqi.fuzail@yahoo.com
+ * byhaqi.fuzail@gmail.com
+ */
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText number, month, year, cvc, name;
-    private String KEY = "Your Key";
-    private Button pay;
-    private CardMultilineWidget cardMultilineWidget;
-    boolean lenNumber = false, lenMonth = false, lenYear = false, lenCVC = false,lenName=false;
+    private EditText mNumber, mMonth, mYear, mCVC, mName;
+    private boolean mLenNumber = false, mLenMonth = false, mLenYear = false, mLenCVC = false, mLenName = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,14 +34,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        number = findViewById(R.id.num);
-        month = findViewById(R.id.month);
-        year = findViewById(R.id.year);
-        cvc = findViewById(R.id.cvc);
-        name = findViewById(R.id.card_name);
-        pay = findViewById(R.id.pay);
+        mNumber = findViewById(R.id.num);
+        mMonth = findViewById(R.id.month);
+        mYear = findViewById(R.id.year);
+        mCVC = findViewById(R.id.cvc);
+        mName = findViewById(R.id.card_name);
+        Button pay = findViewById(R.id.pay);
 
-        number.addTextChangedListener(new FourDigitCardFormatWatcher());
+        mNumber.addTextChangedListener(new FourDigitCardFormatWatcher());
 
         pay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,95 +50,98 @@ public class MainActivity extends AppCompatActivity {
                 hideKeyboard(MainActivity.this);
 
                 if (validateInputData())
-                    payViaStripe();
+                    cardDetails();
             }
         });
     }
 
+    // This validation only checks that the editTexts have the exact lenth we wanted
     private boolean validateInputData() {
-        if (number.getText().toString().length() == 19) {
-            lenNumber = true;
+        if (mNumber.getText().toString().length() == 19) {
+            mLenNumber = true;
         } else
-            number.setError("Invalid Card Number");
+            mNumber.setError("Invalid Card Number");
 
-        if (month.getText().toString().length() > 0) {
-            lenMonth = true;
+        if (mMonth.getText().toString().length() > 0) {
+            mLenMonth = true;
         } else
-            month.setError("Invalid Month");
+            mMonth.setError("Invalid Month");
 
-        if (year.getText().toString().length() == 2) {
-            lenYear = true;
+        if (mYear.getText().toString().length() == 2) {
+            mLenYear = true;
         } else
-            year.setError("Invalid Year");
+            mYear.setError("Invalid Year");
 
-        if (cvc.getText().toString().length() == 3) {
-            lenCVC = true;
+        if (mCVC.getText().toString().length() == 3) {
+            mLenCVC = true;
         } else
-            cvc.setError("Invalid CVC");
+            mCVC.setError("Invalid CVC");
 
-        if(name.getText().toString().length()>0){
-            lenName=true;
-        }else
-            name.setError("Invalid Name");
+        if (mName.getText().toString().length() > 0) {
+            mLenName = true;
+        } else
+            mName.setError("Invalid Name");
 
-        if (lenCVC && lenYear && lenMonth && lenNumber) {
+        if (mLenCVC && mLenYear && mLenMonth && mLenNumber) {
             return true;
         }
         return false;
     }
 
-    private void payViaStripe() {
+    // This methods Creates a card which holds our edittext info
+    private void cardDetails() {
 
-        String num = number.getText().toString().replaceAll("-", "");
+        String num = mNumber.getText().toString().replaceAll("-", "");
         Card.Builder build = new Card.Builder(
                 num,
-                Integer.valueOf(month.getText().toString()),
-                Integer.valueOf(year.getText().toString()),
-                cvc.getText().toString()
+                Integer.valueOf(mMonth.getText().toString()),
+                Integer.valueOf(mYear.getText().toString()),
+                mCVC.getText().toString()
         );
-        build.name(name.getText().toString());
+        build.name(mName.getText().toString());
+        //You can add many more fields like address, pin, etc
         Card card = build.build();
 
         if (!card.validateCVC()) {
-            cvc.setError("Invalid Card CVC");
+            mCVC.setError("Invalid Card CVC");
             return;
         }
         if (!card.validateExpMonth()) {
-            month.setError("Invalid Card Month");
+            mMonth.setError("Invalid Card Month");
             return;
         }
         if (!card.validateNumber()) {
-            number.setError("Invalid Card Number");
+            mNumber.setError("Invalid Card Number");
             return;
         }
-        if(!card.validateExpiryDate()){
-            year.setError("Invalid Card Year");
+        if (!card.validateExpiryDate()) {
+            mYear.setError("Invalid Card Year");
             return;
         }
 
         if (card.validateCard()) { //if card is valid then stripe payment is called
-            callApi(card);
+            stripeCall(card);
         } else {
             Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
         }
 
     }
 
-    private void callApi(Card card) {
+    // This methods hits the Stripe Api to get token
+    private void stripeCall(Card card) {
 
         try {
+            String KEY = "your key";
             Stripe stripe = new Stripe(MainActivity.this, KEY);
             stripe.createToken(
                     card,
                     new TokenCallback() {
                         public void onSuccess(Token token) {
-                            // Send token to your server
+                            // Send token to your server that will manage the further payment handling
                             Toast.makeText(MainActivity.this,
                                     "Successfull payment",
                                     Toast.LENGTH_SHORT
                             ).show();
-
-                            //here you can call your own api from your server and send the token
                         }
 
                         public void onError(Exception error) {
@@ -171,6 +159,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //hides the keyboard
     public static void hideKeyboard(Activity activity) {
         InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         //Find the currently focused view, so we can grab the correct window token from it.
